290e992671a1975a699d608bb254a823e43d9ec9
Veliladon <mburtenshaw@gmail.com>
2026-08-03 19:24:18 -0400

    added infinite grid

--- src/camera.rs
+++ src/camera.rs
@@ -1,10 +1,13 @@
 use crate::prelude::*;
+use bevy::dev_tools::infinite_grid::{InfiniteGrid, InfiniteGridPlugin, InfiniteGridSettings};
 
 pub struct CameraSetupPlugin;
 
 impl Plugin for CameraSetupPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, camera_setup);
+        app.add_plugins(InfiniteGridPlugin)
+            .add_systems(Startup, infinite_grid)
+            .add_systems(Startup, camera_setup);
     }
 }
 
@@ -13,5 +16,14 @@ fn camera_setup(mut commands: Commands) {
         Camera3d::default(),
         Transform::from_xyz(0.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         MeshPickingCamera,
+    ));
+}
+
+fn infinite_grid(mut commands: Commands) {
+    commands.spawn((
+        // You need to spawn an entity with this component
+        InfiniteGrid,
+        // Optional component you can use to configure the grid
+        InfiniteGridSettings::default(),
     ));
 }
